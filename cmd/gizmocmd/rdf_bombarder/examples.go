62e2f30f7708d6a15b4a6e22175e17babae687b8
Vladislav Nikolov <v.nikolov.nn@gmail.com>
2025-11-06 16:44:00 +0200

    docs(gizmocmd): add examples for the rdf bombarder

--- cmd/gizmocmd/rdf_bombarder/examples.go
+++ cmd/gizmocmd/rdf_bombarder/examples.go
@@ -1,5 +1,12 @@
 package rdf_bombarder
 
-var examples = `
-sss
+var examples = `# Sending 10 statements to each of 10 named graphs (100 statements total), 5 at a time, using 10 threads
+graphdbcli gizmo rdf_bombarder -r my_repo \
+--numberOfNamedGraphs 10 \
+--numberOfStatementsPerNamedGraph 10 \
+--numberOfThreads 10 \
+--numberOfStatementsPerRequest 5
+# Using the default value for numberOfNamedGraphs (0) will do nothing 
+# (reserved for future development)
+graphdbcli gizmo rdf_bombarder -r my_repo --numberOfNamedGraphs 0
 `
